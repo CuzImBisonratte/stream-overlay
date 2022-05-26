5fecb0593e9b3e493ef97516475fb25e0ed05cac
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-05-27 00:45:58 +0200

    added height bar chart

--- mobile_controller/script.js
+++ mobile_controller/script.js
@@ -12,3 +12,26 @@ for (var i = 0; i < bg_random_elements.length; i++) {
     // Set background color
     bg_random_elements[i].style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 }
+
+
+
+// Function to show something as chart
+function show_chart(input_percent) {
+
+    // Get screensize in px
+    var screen_height = window.innerHeight;
+
+    // Get element
+    var height_bar = document.getElementById("height_bar");
+
+    // Get 100% - input_percent
+    var height_percentage = 100 - input_percent;
+    height_percentage = height_percentage / 100;
+
+    // Get height in px by multiplying height_percentage with screen_height
+    var height_px = height_percentage * screen_height;
+
+    // Set height as margin top
+    height_bar.style.marginTop = height_px + "px";
+
+}
